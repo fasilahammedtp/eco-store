e38af3c440fd7c5c2388f906615cf6cb91943df3
Fasil <fasilahammedtp@gmail.com>
2026-04-11 13:11:32 +0530

    implemented lazy loading

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import { useAuth } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/cart.css";
 
-function Cart() {
+function Cart() {z
   const [cart, setCart] = useState([]);
   const { loadCartCount } = useContext(CartContext);
   const navigate = useNavigate();
